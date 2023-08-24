91fa08a6b29bc1768cae8ca83d6a2df294795db6
Evgenii Ivanov <eugene.ivanov@sporty.com>
2023-08-23 21:34:51 -0300

    Configure security, add mustache representation

--- src/main/java/com/example/golanceexampleservice/controller/DomainController.java
+++ src/main/java/com/example/golanceexampleservice/controller/DomainController.java
@@ -1,19 +1,32 @@
 package com.example.golanceexampleservice.controller;
 
 import com.example.golanceexampleservice.dto.DomainRequest;
+import com.example.golanceexampleservice.model.Domain;
 import com.example.golanceexampleservice.service.DomainService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping("/domain")
+import java.util.List;
+import java.util.Map;
+
+@Controller
 @RequiredArgsConstructor
 public class DomainController {
 
     private final DomainService domainService;
 
-    @PostMapping
-    public void saveDomain(@RequestBody DomainRequest domainRequest) {
+    @PostMapping("/domain")
+    public ModelAndView saveDomain(Map<String, Object> model, @ModelAttribute("domainRequest") DomainRequest domainRequest) {
         domainService.saveDomain(domainRequest);
+        return getDomains(model);
+    }
+
+    @GetMapping(value = {"/domain","/"})
+    public ModelAndView getDomains(Map<String, Object> model) {
+        List<Domain> domains = domainService.findAll();
+        model.put("domains", domains);
+        return new ModelAndView("index", model);
     }
 }
